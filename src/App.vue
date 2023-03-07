5f015554bb0728952e9b28d6e497df0e1dd670f7
Bogdan <0966312126a@gmail.com>
2023-03-07 05:53:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <BodyApp/>
+</template>
+
+<script>
+
+import {defineComponent} from "vue";
+import BodyApp from "@/components/bodyApp.vue";
+
+export default defineComponent({
+  components: {BodyApp}
+})
+</script>
+
+<style scoped></style>
---
+++ src/components/bodyApp.vue
@@ -0,0 +1,23 @@
+<template>
+  {{myName}}
+</template>
+
+<script>
+import {useCounterStore} from "@/stores/counter";
+import {toRefs} from "vue";
+
+export default {
+  name: 'bodyApp',
+  setup(){
+    const store = useCounterStore()
+    const { myName } = toRefs(store)
+
+    return {
+      myName
+    }
+  }
+
+}
+</script>
+
+<style scoped></style>
