5f0b9fe6ff45566c9e83fce6ab58631b8b9fed48
bogdan <0966312126a@gmail.com>
2023-03-30 03:44:27 +0300

    #

--- src/components/HomeNewsCard.vue
+++ src/components/HomeNewsCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hover:underline mx-auto border relative p-3 w-3/4">
     <div>
-      <p class="text-center mb-4">Джерело: {{ itemNew.author }}</p>
+      <p class="text-center mb-4" v-if="itemNew.author">Автор: {{ itemNew.author }}</p>
       <p>{{ itemNew.title }}</p>
     </div>
     <a class="inset-0 absolute" :href="itemNew.url" target="_blank"> </a>
