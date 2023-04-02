8879474cd407b5919b7585c5394be1cea1582502
bogdan <0966312126a@gmail.com>
2023-04-02 19:32:52 +0300

    #

--- src/components/newsCategoryItem.vue
+++ src/components/newsCategoryItem.vue
@@ -2,10 +2,11 @@
   <p class="text-blue-500 text-center text-xl">
     Новини категорії: {{ categoriesName }}
   </p>
-  <div class="text-center text-sm my-5" v-if="!Object.keys(dataCard).length">
-    Пусто
-  </div>
-  <div class="news-grid" v-else>
+  <div class="text-center text-sm my-5" v-if="!dataCard.length">Пусто</div>
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2.5 my-5"
+    v-else
+  >
     <HomeNewsCard v-for="(item, idx) in dataCard" :key="idx" :item-new="item" />
   </div>
 </template>
@@ -20,8 +21,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.news-grid {
-  @apply grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2.5 my-5;
-}
-</style>
+<style scoped></style>
