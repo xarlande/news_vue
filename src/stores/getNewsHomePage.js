1337ffe9edbb6f1306d57d86edbdead22e9102ee
bogdan <0966312126a@gmail.com>
2023-04-02 19:23:40 +0300

    #

--- src/stores/getNewsHomePage.js
+++ src/stores/getNewsHomePage.js
@@ -1,7 +1,6 @@
 import { defineStore } from "pinia";
 const apiUrl = "https://newsapi.org/v2";
 const apiKey = "6bd4a18884a9402d81e67a053cbb9cac";
-
 export const useNewsHomePage = defineStore("HomeNews", {
   state: () => ({
     newsPopular: [],
@@ -11,43 +10,18 @@ export const useNewsHomePage = defineStore("HomeNews", {
   }),
   actions: {
     getAllNews() {
-      this.getNewsTechnology();
-      this.getNewsBusiness();
-      this.getNewsSport();
-      this.getNewsPopular();
+      this.getNews("technology", "newsTechnology");
+      this.getNews("business", "newsBusiness");
+      this.getNews("sport", "newsSport");
+      this.getNews("", "newsPopular");
     },
-    getNewsTechnology() {
+    getNews(categoryNews, stateKey) {
       fetch(
-        `${apiUrl}/top-headlines?country=ua&category=technology&apiKey=${apiKey}`
+        `${apiUrl}/top-headlines?country=ua&category=${categoryNews}&apiKey=${apiKey}`
       )
         .then((r) => r.json())
         .then((data) => {
-          this.newsTechnology = data.articles;
-        });
-    },
-    getNewsSport() {
-      fetch(
-        `${apiUrl}/top-headlines?country=ua&category=sport&apiKey=${apiKey}`
-      )
-        .then((r) => r.json())
-        .then((data) => {
-          this.newsSport = data.articles;
-        });
-    },
-    getNewsBusiness() {
-      fetch(
-        `${apiUrl}/top-headlines?country=ua&category=business&apiKey=${apiKey}`
-      )
-        .then((r) => r.json())
-        .then((data) => {
-          this.newsBusiness = data.articles;
-        });
-    },
-    getNewsPopular() {
-      fetch(`${apiUrl}/top-headlines?country=ua&apiKey=${apiKey}`)
-        .then((r) => r.json())
-        .then((data) => {
-          this.newsPopular = data.articles;
+          this[stateKey] = data.articles;
         });
     },
   },
