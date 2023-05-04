53c34c936f6c2e0080fa5b6661921d07bebd2414
bogdan <0966312126a@gmail.com>
2023-05-04 21:32:38 +0300

    create error page

---
+++ error.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Error code: {{ error.statusCode }}</div>
+  <div>{{ error.statusMessage }}</div>
+  <NuxtLink to="/">Go Home</NuxtLink>
+</template>
+<script setup>
+const error = useError();
+</script>
