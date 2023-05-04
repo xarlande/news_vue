c10415b5229ac882caf2d7667edf208210c6109e
bogdan <0966312126a@gmail.com>
2023-05-04 19:51:30 +0300

    rewrite store(composition api)

--- src/stores/getNewsForHome.js
+++ src/stores/getNewsForHome.js
@@ -1,33 +1,33 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 const apiUrl = "https://newsapi.org/v2";
 const apiKey = "6bd4a18884a9402d81e67a053cbb9cac";
-export const useNewsHomePage = defineStore("StoreForHome", {
-  state: () => ({
-    newsPopular: [],
-    newsBusiness: [],
-    newsSport: [],
-    newsTechnology: [],
-  }),
-  actions: {
-    getAllNews() {
-      this.getNews("technology", "newsTechnology");
-      this.getNews("business", "newsBusiness");
-      this.getNews("sport", "newsSport");
-      this.getNews("", "newsPopular");
-    },
-    getNews(categoryNews, stateKey) {
-      fetch(
-        `${apiUrl}/top-headlines?country=ua&category=${categoryNews}&apiKey=${apiKey}`
-      )
-        .then((r) => r.json())
-        .then((data) => {
-          if (data.status === "ok") {
-            this[stateKey] = data.articles;
-          }
-        })
-        .catch((data) => {
-          console.log(`error ${data}`);
-        });
-    },
-  },
+export const useNewsHomePage = defineStore("StoreForHome", () => {
+  const newsPopular = ref([]);
+  const newsBusiness = ref([]);
+  const newsSport = ref([]);
+  const newsTechnology = ref([]);
+
+  const getAllNews = () => {
+    getNews("technology", newsTechnology);
+    getNews("business", newsBusiness);
+    getNews("sport", newsSport);
+    getNews("", newsPopular);
+  };
+  const getNews = (categoryNews, stateKey) => {
+    fetch(
+      `${apiUrl}/top-headlines?country=ua&category=${categoryNews}&apiKey=${apiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "ok") {
+          stateKey.value = data.articles;
+        }
+      })
+      .catch((data) => {
+        console.log(`error ${data}`);
+      });
+  };
+
+  return { newsPopular, newsBusiness, newsSport, newsTechnology, getAllNews };
 });
