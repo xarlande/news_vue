86219d3d5cc15052e1eb2b220820149448795c2a
Bohdan Velhan <bohdanvelhan@MacBook-Pro-Bohdan.local>
2026-05-25 19:14:13 +0300

    update

--- components/NewsCategory/NewsCategoryCard.vue
+++ components/NewsCategory/NewsCategoryCard.vue
@@ -1,29 +1,27 @@
 <template>
-  <div
-    class="hover:underline mx-auto border rounded relative p-3 flex flex-col justify-center"
-  >
-    <nuxt-img
-      v-if="itemNew.urlToImage"
-      class="mx-auto"
-      sizes="sm:100vw md:50vw lg:400px"
-      format="webp"
-      quality="80"
-      :src="itemNew.urlToImage"
-      alt="image"
-    />
-    <div class="my-2 text-center">
-      <p>{{ itemNew.title }}</p>
+    <div
+        class="hover:underline mx-auto border rounded relative p-3 flex flex-col justify-center"
+    >
+        <img
+            v-if="itemNew.urlToImage"
+            class="mx-auto"
+            :src="itemNew.urlToImage"
+            alt="image"
+            loading="lazy"
+        />
+        <div class="my-2 text-center">
+            <p>{{ itemNew.title }}</p>
+        </div>
+        <a class="inset-0 absolute" :href="itemNew.url" target="_blank"> </a>
     </div>
-    <a class="inset-0 absolute" :href="itemNew.url" target="_blank"> </a>
-  </div>
 </template>
 
 <script setup>
 defineProps({
-  itemNew: {
-    type: Object,
-    default: () => {},
-  },
+    itemNew: {
+        type: Object,
+        default: () => {},
+    },
 });
 </script>
 
